ab1ef20d594ea5f80085c86816f5f4531a94bf4d
Snarko6 <165910445+Snarko6@users.noreply.github.com>
2024-04-03 16:18:27 +0200

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -172,7 +172,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['سناركو'] 
+handler.command = ['مينيو'] 
 
 export default handler
 function clockString(ms) {
